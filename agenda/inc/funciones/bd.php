fad31bf11a0c060f22f5ec4c69ca4d28add612f6
sebastian <spichardo@goplek.com>
2019-01-21 14:59:24 -0600

    Fix little bugs

--- agenda/inc/funciones/bd.php
+++ agenda/inc/funciones/bd.php
@@ -1,18 +1,19 @@
 <?php
 
 // credenciales de la base de datos
-$servidor = "localhost";
-$bd = "dump";
+$servidor = "127.0.0.1";
+$bd = "dump.sql";
 $usuario = "root";
 $password = "";
 
 $conn = new mysqli($servidor,$usuario,$password,$bd);
 
-//if($conn->connect_errno)
-//{
-//    echo "no conecto";
-//}
-//else
-//{
-//    echo "conecto";
-//}
+/*if($conn->connect_errno)
+{
+    echo "no conecto";
+    echo  $conn->connect_error;
+}
+else
+{
+    echo "conecto";
+}*/
